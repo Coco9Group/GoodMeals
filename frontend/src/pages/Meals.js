0ee391e49ca460ac1246e752331959d6a66c22ca
Erica <orange7961@gmail.com>
2020-11-11 03:28:09 -0500

    remove meal css

---
+++ frontend/src/pages/Meals.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import Meal from '../components/Meal';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+
+export default function Meals() {
+
+}
