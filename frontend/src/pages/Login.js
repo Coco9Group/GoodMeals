86e1eed2b036240cbf67a25fa58136881a0d0200
Erica <orange7961@gmail.com>
2020-11-13 00:40:23 -0500

    add email pw invaild message

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { login } from '../actions/userActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 import "./Login.css";
 
 export default function Login(props) {
@@ -11,7 +13,7 @@ export default function Login(props) {
     const redirect = props.location.search ? props.location.search.split('=')[1] : '/';
 
     const userLogin = useSelector((state) => state.userLogin);
-    const { userInfo } = userLogin;
+    const { userInfo, loading, error } = userLogin;
 
     const dispatch = useDispatch();
     const submitHandler = (e) => {
@@ -31,8 +33,10 @@ export default function Login(props) {
                 <div>
                     <h1>Log In</h1>
                 </div>
+                {loading && <LoadingBox></LoadingBox>}
+                {error && <MessageBox variant="danger">{error}</MessageBox>}
                 <div>
-                    <label htmlFor="email">Email Address</label>
+                    <label htmlFor="email">Email</label>
                     <input type="email"
                         id="email"
                         placeholder="Email"
