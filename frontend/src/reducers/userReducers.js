ee4abc0b7fb6aa035431332c22c7e8b3959e109a
Erica <orange7961@gmail.com>
2020-11-16 00:29:10 -0500

    user profile function

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,7 +1,4 @@
 import {
-    // USER_DETAILS_FAIL,
-    // USER_DETAILS_REQUEST,
-    // USER_DETAILS_SUCCESS,
     USER_SIGNUP_FAIL,
     USER_SIGNUP_REQUEST,
     USER_SIGNUP_SUCCESS,
@@ -9,10 +6,13 @@ import {
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
     USER_LOGOUT,
-    // USER_UPDATE_PROFILE_FAIL,
-    // USER_UPDATE_PROFILE_REQUEST,
-    // USER_UPDATE_PROFILE_RESET,
-    // USER_UPDATE_PROFILE_SUCCESS,
+    USER_DETAILS_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_UPDATE_PROFILE_FAIL,
+    USER_UPDATE_PROFILE_REQUEST,
+    USER_UPDATE_PROFILE_RESET,
+    USER_UPDATE_PROFILE_SUCCESS,
 } from '../constants/userConstants';
 
 export const userLoginReducer = (state = {}, action) => {
@@ -42,3 +42,32 @@ export const userSignupReducer = (state = {}, action) => {
             return state;
     }
 };
+
+export const userDetailsReducer = (state = { loading: true }, action) => {
+    switch (action.type) {
+        case USER_DETAILS_REQUEST:
+            return { loading: true };
+        case USER_DETAILS_SUCCESS:
+            return { loading: false, user: action.payload };
+        case USER_DETAILS_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
+
+export const userUpdateProfileReducer = (state = {}, action) => {
+    switch (action.type) {
+        case USER_UPDATE_PROFILE_REQUEST:
+            return { loading: true };
+        case USER_UPDATE_PROFILE_SUCCESS:
+            return { loading: false, success: true };
+        case USER_UPDATE_PROFILE_FAIL:
+            return { loading: false, error: action.payload };
+        case USER_UPDATE_PROFILE_RESET:
+            return {};
+        default:
+            return state;
+    }
+};
+
