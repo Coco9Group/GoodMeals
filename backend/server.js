158ec93adc899a38d4ea0e51a17c0d8909119fa6
Erica <orange7961@gmail.com>
2021-02-05 13:33:37 -0500

    publish to heroku3

--- backend/server.js
+++ backend/server.js
@@ -30,16 +30,16 @@ app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID || 'sandbox');
 });
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
-// app.use(express.static(path.join(__dirname, '/frontend/build')));
-// app.get('*', (req, res) =>
-//     res.sendFile(path.join(__dirname, 'frontend/build/index.html'))
-// );
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
-app.get('/', (req, res) => {
-    res.send('server ready');
-});
+// app.get('/', (req, res) => {
+//     res.send('server ready');
+// });
 
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message });
