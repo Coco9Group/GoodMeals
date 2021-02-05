258495c681f7f4737d929c25ba5953cef26e50cd
Erica <orange7961@gmail.com>
2021-02-05 13:22:03 -0500

    publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -30,9 +30,16 @@ app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID || 'sandbox');
 });
 
-// app.get('/', (req, res) => {
-//     res.send('server ready');
-// });
+// const __dirname = path.resolve();
+
+// app.use(express.static(path.join(__dirname, '/frontend/build')));
+// app.get('*', (req, res) =>
+//     res.sendFile(path.join(__dirname, 'frontend/build/index.html'))
+// );
+
+app.get('/', (req, res) => {
+    res.send('server ready');
+});
 
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message });
